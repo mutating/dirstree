2c8de811a0b1e985f576d60aeaf1d5999eb1ab07
esblinov <esblinov@avito.ru>
2025-09-27 23:49:14 +0300

    Sort walker output for deterministic test order

--- tests/test_walker.py
+++ tests/test_walker.py
@@ -9,12 +9,18 @@ from dirstree import DirectoryWalker
 def test_walk_test_directory_with_default_python_extensions(walk_directory_path: Union[str, Path]):
     walker = DirectoryWalker(walk_directory_path)
 
-    assert [str(x) for x in walker.walk()] == [
+    expected_paths = [
         os.path.join('tests', 'test_files', 'walk_it', '__init__.py'),
         os.path.join('tests', 'test_files', 'walk_it', 'simple_code.py'),
         os.path.join('tests', 'test_files', 'walk_it', 'nested_folder', 'python_file.py'),
         os.path.join('tests', 'test_files', 'walk_it', 'nested_folder', '__init__.py'),
     ]
+    real_paths = [str(x) for x in walker.walk()]
+
+    expected_paths.sort()
+    real_paths.sort()
+
+    assert real_paths == expected_paths
 
 
 def test_walk_test_directory_with_txt_extension(walk_directory_path: Union[str, Path]):
