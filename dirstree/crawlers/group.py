4b33b5c834087c3462a744b058c28bedb24cead6
esblinov <esblinov@avito.ru>
2025-10-07 14:23:11 +0300

    Add __repr__ to CrawlersGroup

--- dirstree/crawlers/group.py
+++ dirstree/crawlers/group.py
@@ -2,6 +2,7 @@ from typing import List, Generator
 from pathlib import Path
 
 from cantok import AbstractToken, DefaultToken
+from printo import descript_data_object
 
 from dirstree.crawlers.abstract import AbstractCrawler
 
@@ -9,6 +10,13 @@ from dirstree.crawlers.abstract import AbstractCrawler
 class CrawlersGroup(AbstractCrawler):
     def __init__(self, crawlers: List[AbstractCrawler]) -> None:
         self.crawlers = crawlers
+
+    def __repr__(self):
+        return descript_data_object(
+            type(self).__name__,
+            (self.crawlers,),
+            {},
+        )
 
     def go(self, token: AbstractToken = DefaultToken()) -> Generator[Path, None, None]:
         memory = set()
