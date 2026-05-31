4342041825a30a01a5ebe736b7e58925755c0c05
pomponchik <zheni-b@yandex.ru>
2026-05-31 04:07:17 +0300

    Add the freeze flag to the python crawler

--- dirstree/crawlers/python_crawler.py
+++ dirstree/crawlers/python_crawler.py
@@ -13,9 +13,10 @@ class PythonCrawler(Crawler):
         exclude: Optional[List[str]] = None,
         filter: Optional[Callable[[Path], bool]] = None,  # noqa: A002
         token: AbstractToken = DefaultToken(),  # noqa: B008
+        freeze: bool = False,
     ) -> None:
         super().__init__(
-            *paths, extensions=('.py',), exclude=exclude, filter=filter, token=token,
+            *paths, extensions=('.py',), exclude=exclude, filter=filter, token=token, freeze=freeze,
         )
         self.addictional_repr_filters = {
             'extensions': lambda x: False,  # noqa: ARG005
