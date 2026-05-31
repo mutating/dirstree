6f4f4ceaaa948d52a168a5b3e742f6ab506b1f50
pomponchik <zheni-b@yandex.ru>
2026-06-01 00:43:11 +0300

    raise_on_cancel option's support

--- dirstree/crawlers/python_crawler.py
+++ dirstree/crawlers/python_crawler.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Callable, List, Optional, Union
+from typing import Callable, List, Optional, Type, Union
 
 from cantok import AbstractToken, DefaultToken
 
@@ -14,9 +14,10 @@ class PythonCrawler(Crawler):
         filter: Optional[Callable[[Path], bool]] = None,  # noqa: A002
         token: AbstractToken = DefaultToken(),  # noqa: B008
         freeze: bool = False,
+        raise_on_cancel: Union[bool, BaseException, Type[BaseException]] = False,
     ) -> None:
         super().__init__(
-            *paths, extensions=('.py',), exclude=exclude, filter=filter, token=token, freeze=freeze,
+            *paths, extensions=('.py',), exclude=exclude, filter=filter, token=token, freeze=freeze, raise_on_cancel=raise_on_cancel,
         )
         self.addictional_repr_filters = {
             'extensions': lambda x: False,  # noqa: ARG005
